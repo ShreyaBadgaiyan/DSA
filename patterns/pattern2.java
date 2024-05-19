d11693006a1198d2d1839ba54288b5496f64eb7a
Shreya Badgaiyan <117737111+ShreyaBadgaiyan@users.noreply.github.com>
2024-05-19 17:13:19 +0530

    pattern2

--- patterns/pattern2.java
+++ patterns/pattern2.java
@@ -2,12 +2,12 @@ package patterns;
 
 import java.util.Scanner;
 
-public class pattern1{
-    public static void main(String[] args){
+public class pattern2 {
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print('*');
             }
             System.out.println();
