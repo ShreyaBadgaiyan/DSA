83a73fb5949d45f1a20e297613450cc93fe4f730
Shreya Badgaiyan <117737111+ShreyaBadgaiyan@users.noreply.github.com>
2024-05-22 10:47:48 +0530

    pattern11 done

---
+++ patterns/pattern11.java
@@ -0,0 +1,24 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class pattern11 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print(1 );
+                }
+                
+                else{
+                    System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }//start with 1 else 0 this can also be an algorithm
+    
+}
