8caec8808167a7cdd490d967619109fed48a5118
Shreya Badgaiyan <117737111+ShreyaBadgaiyan@users.noreply.github.com>
2024-06-02 00:31:45 +0530

    problem5

--- Recursion/rec5.java
+++ Recursion/rec5.java
@@ -2,24 +2,24 @@ package Recursion;
 
 import java.util.Scanner;
 
-public class rec4 {
-// USED BACKTRACKING IN THIS CASE WHEREIN WE DID THE TASK AFTER FUNCTION CALLING.
-    
+public class rec5 {
+
+    //Reverse from N to 1 using Back Tracking
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
 
-        Print(n,n);
+        Print(1,n);
     }
 
     public static void Print(int count,int a){
 
-        if(count<1){
+        if(count>a){
             return;
         }
-        Print(count-1,a);
+        Print(count+1,a);
+
         System.out.println(count);
-       
         
         
 
